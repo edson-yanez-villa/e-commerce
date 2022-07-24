02e00c3632a4130ce53f30d78c944aa89c16af3c
Moises Edson Yañez Villa <yanezvilla.edson@gmail.com>
2022-07-24 00:48:03 -0400

    Se agregaron las validaciones para el login

--- services/validations.js
+++ services/validations.js
@@ -23,13 +23,13 @@ const errorMessage = {
         valueMissing: "El campo Nombre no puede estar vacio",
     },
     email: {
-        valueMissing: "Este campo no puede estar vacio",
+        valueMissing: "El campo email no puede estar vacio",
         typeMismatch: "El correo no es valido.",
     },
     password: {
-        valueMissing: "Este campo no puede estar vacio",
+        valueMissing: "El campo password no puede estar vacio",
         patternMismatch:
-            "Al menos 6 caracteres, máximo 12, debe contener una letra minúscula, una letra mayúscula, un número y no puede contener caracteres especiales.",
+            "Debe contener una letra minúscula, mayúscula, un número.",
     },
     message: {
         valueMissing:
