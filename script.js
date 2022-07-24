ca5a41986b1e63152ee7bd2234ece0b82ef27f1f
Moises Edson Yañez Villa <yanezvilla.edson@gmail.com>
2022-07-24 00:13:16 -0400

    Se agrego las validaciones para el rodapie de contacto

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 import { clickButton } from "./services/clickButtons.js";
+import { validate } from "./services/validations.js";
 
-const buttons = document.querySelectorAll("button")
+const buttons = document.querySelectorAll("button");
+const inputs = document.querySelectorAll("input");
+const textAreas = document.querySelectorAll("textarea");
 
-buttons.forEach(button => {
+buttons.forEach((button) => {
     button.addEventListener("click", (button) => {
         clickButton(button.target);
+    });
+});
+
+inputs.forEach((input) => {
+    input.addEventListener("blur", (input) => {
+        validate(input.target);
+    });
+});
+
+textAreas.forEach((input) => {
+    input.addEventListener("blur", (input) => {
+        validate(input.target);
     });
 });
--- services/validations.js
+++ services/validations.js
@@ -1,3 +1,48 @@
 export function validate(input) {
     const inputType = input.dataset.tipo;
+    if (input.validity.valid) {
+        input.parentElement.classList.remove("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML =
+            "";
+    } else {
+        input.parentElement.classList.add("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML =
+            showErrorMessage(inputType, input);
+    }
+}
+
+const typeErrors = [
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
+    "customError",
+];
+
+const errorMessage = {
+    name: {
+        valueMissing: "El campo Nombre no puede estar vacio",
+    },
+    email: {
+        valueMissing: "Este campo no puede estar vacio",
+        typeMismatch: "El correo no es valido.",
+    },
+    password: {
+        valueMissing: "Este campo no puede estar vacio",
+        patternMismatch:
+            "Al menos 6 caracteres, máximo 12, debe contener una letra minúscula, una letra mayúscula, un número y no puede contener caracteres especiales.",
+    },
+    message: {
+        valueMissing:
+            "El campo mensaje debe contener almenos 10 a 120 caracteres.",
+    },
+};
+
+function showErrorMessage(inputType, input) {
+    let message = "";
+    typeErrors.forEach((error) => {
+        if (input.validity[error]) {
+            message = errorMessage[inputType][error];
+        }
+    });
+    return message;
 }
