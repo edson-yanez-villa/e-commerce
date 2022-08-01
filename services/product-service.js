9ade463aef173567732b2327ef641fef5c8d87d6
Moises Edson Yañez Villa <yanezvilla.edson@gmail.com>
2022-08-01 07:21:08 -0400

    Se agrego el crud completo en services

--- services/product-service.js
+++ services/product-service.js
@@ -18,7 +18,40 @@ const crearProducto = (image, section, name, price, description) => {
     });
 };
 
+const removeProduct = (id) => {
+    return fetch(`http://localhost:3000/product/${id}`, {
+        method: "DELETE",
+    });
+};
+
+const detailProduct = (id) => {
+    return fetch(`http://localhost:3000/product/${id}`).then((respuesta) =>
+        respuesta.json()
+    );
+};
+
+const updateProduct = (image, section, name, price, description, id) => {
+    return fetch(`http://localhost:3000/product/perfil/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            name,
+            image,
+            price,
+            section,
+            description,
+        }),
+    })
+        .then((respuesta) => respuesta)
+        .catch((err) => console.log(err));
+};
+
 export const productServices = {
     productList,
     crearProducto,
+    removeProduct,
+    detailProduct,
+    updateProduct,
 };
