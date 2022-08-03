1e8a170cb97056eac4e4edf3b0ae1bfc61bc1f17
Moises Edson Yañez Villa <yanezvilla.edson@gmail.com>
2022-08-02 22:34:39 -0400

    Se agrego los metodos para actualuzar un producto

--- controllers/update-controller.js
+++ controllers/update-controller.js
@@ -7,24 +7,52 @@ const price = document.querySelector("[data-price]");
 const description = document.querySelector("[data-description]");
 
 const renderProduct = async () => {
-  const url = new URL(window.location);
-  const id = url.searchParams.get("id");
-  if (id === null) {
-    window.location.href = "index.html";
-  }
+    const url = new URL(window.location);
+    const id = url.searchParams.get("id");
+    if (id === null) {
+        window.location.href = "index.html";
+    }
 
-  try {
-    const product = await productServices.detailProduct(id);
-    image.value = product.image;
-    section.value = product.section;
-    name.value = product.name;
-    price.value = product.price;
-    description.value = product.description;
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        const product = await productServices.detailProduct(id);
+        image.value = product.image;
+        section.value = product.section;
+        name.value = product.name;
+        price.value = product.price;
+        description.value = product.description;
+    } catch (error) {
+        console.log(error);
+    }
 };
 
-const updateProducto = (id) => {};
+const actualizarProducto = (id) => {
+    productServices
+        .updateProduct(
+            image.value,
+            section.value,
+            name.value,
+            price.value,
+            description.value,
+            id
+        )
+        .then(() => alert("Producto actualizado"))
+        .catch((error) => alert(`ocurrio un error ${error}`));
+};
+
+const button = document.querySelector("[product-update]");
+button.addEventListener("click", (event) => {
+    const url = new URL(window.location);
+    const id = url.searchParams.get("id");
+    if (id === null) {
+        window.location.href = "index.html";
+    }
+    actualizarProducto(id);
+    image.value = "";
+    section.value = "";
+    name.value = "";
+    price.value = "";
+    description.value = "";
+    window.location.href = "index.html";
+});
 
 renderProduct();
